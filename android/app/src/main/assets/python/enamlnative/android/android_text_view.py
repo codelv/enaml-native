0b132354cf9b836e2f0d3fdd1bb2a8267d6ab23f
frmdstryr <frmdstryr@gmail.com>
2017-06-07 12:36:24 -0400

    Implement more text options

--- android/app/src/main/assets/python/enamlnative/android/android_text_view.py
+++ android/app/src/main/assets/python/enamlnative/android/android_text_view.py
@@ -14,8 +14,9 @@ from atom.api import Typed
 
 from enamlnative.widgets.text_view import ProxyTextView
 
-from .android_view import AndroidView
+from .android_view import AndroidView, Color
 
+Typeface = jnius.autoclass('android.graphics.Typeface')
 TextView = jnius.autoclass('android.widget.TextView')
 
 class TextWatcher(jnius.PythonJavaClass):
@@ -67,13 +68,68 @@ class AndroidTextView(AndroidView, ProxyTextView):
         d = self.declaration
         self.set_text(d.text)
 
+        if d.all_caps:
+            self.set_all_caps(d.all_caps)
+        if d.auto_link_mask:
+            self.set_auto_link_mask(d.auto_link_mask)
+        if d.font_family or d.font_style:
+            self.update_font()
+        if d.text_size:
+            self.set_text_size(d.text_size)
+        if d.text_color:
+            self.set_text_color(d.text_color)
+        if d.link_color:
+            self.set_link_color(d.link_color)
+        if d.highlight_color:
+            self.set_highlight_color(d.highlight_color)
+        if d.lines:
+            self.set_lines(d.lines)
+        if d.max_lines:
+            self.set_max_lines(d.max_lines)
+
     #--------------------------------------------------------------------------
     # ProxyTextView API
     #--------------------------------------------------------------------------
+    def set_all_caps(self, enabled):
+        self.widget.setAllCaps(enabled)
+
+    def set_auto_link_mask(self, mask):
+        self.widget.setAutoLinkMask(mask)
+
+    def set_font_family(self, family):
+        self.update_font()
+
+    def set_font_style(self, style):
+        self.update_font()
+
+    def update_font(self):
+        d = self.declaration
+        style = getattr(Typeface, d.font_style.upper() or 'NORMAL')
+        tf = Typeface.create(d.font_family or None, style)
+        self.widget.setTypeface(tf)
+
     def set_text(self, text):
         """ Set the text in the widget.
 
         """
         self.widget.setText(text, 0, len(text))
+
+    def set_text_color(self, color):
+        self.widget.setTextColor(Color.parseColor(color))
+
+    def set_highlight_color(self, color):
+        self.widget.setHighlightColor(Color.parseColor(color))
+
+    def set_link_color(self, color):
+        self.widget.setLinkTextColor(Color.parseColor(color))
+
+    def set_text_size(self, size):
+        self.widget.setTextSize(size)
+
+    def set_lines(self, lines):
+        self.widget.setLines(lines)
+
+    def set_max_lines(self, lines):
+        self.widget.setMaxLines(lines)
 
 
--- android/app/src/main/assets/python/enamlnative/widgets/text_view.py
+++ android/app/src/main/assets/python/enamlnative/widgets/text_view.py
@@ -10,7 +10,7 @@ Created on May 20, 2017
 @author: jrm
 '''
 from atom.api import (
-    Typed, ForwardTyped, Unicode, Enum, Event, observe, set_default
+    Typed, ForwardTyped, Unicode, Enum, Float, Int, Bool, observe, set_default
 )
 
 from enaml.core.declarative import d_
@@ -25,35 +25,77 @@ class ProxyTextView(View):
     #: A reference to the Label declaration.
     declaration = ForwardTyped(lambda: TextView)
 
+    def set_all_caps(self, enabled):
+        raise NotImplementedError
+
+    def set_auto_link_mask(self, mask):
+        raise NotImplementedError
+
+    def set_font_family(self, family):
+        raise NotImplementedError
+
+    def set_font_style(self, style):
+        raise NotImplementedError
+
     def set_text(self, text):
         raise NotImplementedError
 
-    # def set_align(self, align):
-    #     raise NotImplementedError
-    #
-    # def set_vertical_align(self, align):
-    #     raise NotImplementedError
+    def set_text_color(self, color):
+        raise NotImplementedError
+
+    def set_highlight_color(self, color):
+        raise NotImplementedError
+
+    def set_link_color(self, color):
+        raise NotImplementedError
+
+    def set_text_size(self, size):
+        raise NotImplementedError
+
+    def set_lines(self, lines):
+        raise NotImplementedError
+
+    def set_max_lines(self, lines):
+        raise NotImplementedError
 
 
 class TextView(View):
     """ A simple control for displaying read-only text.
 
     """
+
+    #: Sets the properties of this field to transform input to ALL CAPS display.
+    all_caps = d_(Bool())
+
+    #: Sets the autolink mask of the text.
+    auto_link_mask = d_(Int(0))
+
+    #: Font family
+    font_family = d_(Unicode())
+
+    #: Font style
+    font_style = d_(Enum('normal','bold','italic','bold_italic'))
+
+    #: Sets the color used to display the selection highlight.
+    highlight_color = d_(Unicode())
+
+    #: Sets the color of links in the text.
+    link_color = d_(Unicode())
+
     #: The unicode text for the label.
     text = d_(Unicode())
 
-    # #: The horizontal alignment of the text in the widget area.
-    # align = d_(Enum('left', 'right', 'center', 'justify'))
-    #
-    # #: The vertical alignment of the text in the widget area.
-    # vertical_align = d_(Enum('center', 'top', 'bottom'))
-    #
-    # #: An event emitted when the user clicks a link in the label.
-    # #: The payload will be the link that was clicked.
-    # link_activated = d_(Event(), writable=False)
-    #
-    # #: Labels hug their width weakly by default.
-    # hug_width = set_default('weak')
+    #: Sets the text color for all the states (normal, selected, focused) to be this color.
+    text_color = d_(Unicode())
+
+    #: Set the default text size to the given value, interpreted as "scaled pixel" units.
+    text_size = d_(Float(strict=False))
+
+    #: Sets the height of the TextView to be exactly lines tall.
+    lines = d_(Int())
+
+    #: Sets the height of the TextView to be at most maxLines tall.
+    max_lines = d_(Int())
 
     #: A reference to the ProxyLabel object.
     proxy = Typed(ProxyTextView)
@@ -61,7 +103,11 @@ class TextView(View):
     #--------------------------------------------------------------------------
     # Observers
     #--------------------------------------------------------------------------
-    @observe('text')#, 'align', 'vertical_align')
+    @observe('all_caps', 'auto_link_mask',
+             'font_family', 'font_style',
+             'text', 'text_color', 'text_size',
+             'link_color', 'highlight_color',
+             'lines', 'max_lines')
     def _update_proxy(self, change):
         """ An observer which sends the state change to the proxy.
 
