4b0cc47173ec0b0826d7ffdc57e43bee6a321245
frmdstryr <frmdstryr@gmail.com>
2017-06-21 16:15:12 -0400

    Add speedups to zope.interfaces

--- python-for-android/pythonforandroid/recipes/zope_interface/__init__.py
+++ python-for-android/pythonforandroid/recipes/zope_interface/__init__.py
@@ -1,8 +1,9 @@
-from pythonforandroid.toolchain import PythonRecipe, current_directory
+from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
+from pythonforandroid.toolchain import current_directory
 import sh
 
 
-class ZopeInterfaceRecipe(PythonRecipe):
+class ZopeInterfaceRecipe(CppCompiledComponentsPythonRecipe):
     call_hostpython_via_targetpython = False
     name = 'zope_interface'
     version = '4.1.3'
@@ -10,13 +11,17 @@ class ZopeInterfaceRecipe(PythonRecipe):
     site_packages_name = 'zope.interface'
 
     depends = [('python2' ,'python2crystax')]
-    patches = ['no_tests.patch']
+    patches = ['no_tests.patch', 'force_build_ext.patch']
 
     def prebuild_arch(self, arch):
         super(ZopeInterfaceRecipe, self).prebuild_arch(arch)
         with current_directory(self.get_build_dir(arch.arch)):
             sh.rm('-rf', 'src/zope/interface/tests', 'src/zope/interface/common/tests')
-            #sh.touch('src/zope/__init__.py')
-
+            
+    def postbuild_arch(self, arch):
+        super(ZopeInterfaceRecipe, self).postbuild_arch(arch)
+        with current_directory(self.ctx.get_site_packages_dir(arch)):
+            sh.touch( 'zope/__init__.py')
+        
 
 recipe = ZopeInterfaceRecipe()
