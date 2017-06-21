eaba4733f8178cd02c802f43ce1808c52bb73aca
frmdstryr <frmdstryr@gmail.com>
2017-06-21 15:27:07 -0400

    Update twisted and remove tests

--- python-for-android/pythonforandroid/recipes/twisted/__init__.py
+++ python-for-android/pythonforandroid/recipes/twisted/__init__.py
@@ -15,15 +15,15 @@ class TwistedRecipe(CythonRecipe):
     version = '16.0.0'
     url = 'https://github.com/twisted/twisted/archive/twisted-{version}.tar.gz'
 
-    depends = ['setuptools', 'zope_interface']
-
+    depends = [ 'zope_interface']
     call_hostpython_via_targetpython = False
-    install_in_hostpython = True
 
     def prebuild_arch(self, arch):
         super(TwistedRecipe, self).prebuild_arch(arch)
         # TODO Need to whitelist tty.pyo and termios.so here
-        print('Should remove twisted tests etc. here, but skipping for now')
+        with current_directory(self.get_build_dir(arch.arch)):
+            sh.rm('-rf', glob.glob('twisted/*/test'))
+            sh.rm('-f',glob.glob('twisted/test/*.py'))
 
     def get_recipe_env(self, arch):
         env = super(TwistedRecipe, self).get_recipe_env(arch)
--- python-for-android/pythonforandroid/recipes/zope_interface/__init__.py
+++ python-for-android/pythonforandroid/recipes/zope_interface/__init__.py
@@ -9,13 +9,14 @@ class ZopeInterfaceRecipe(PythonRecipe):
     url = 'https://pypi.python.org/packages/source/z/zope.interface/zope.interface-{version}.tar.gz'
     site_packages_name = 'zope.interface'
 
-    depends = ['python2']
+    depends = [('python2' ,'python2crystax')]
     patches = ['no_tests.patch']
 
     def prebuild_arch(self, arch):
         super(ZopeInterfaceRecipe, self).prebuild_arch(arch)
         with current_directory(self.get_build_dir(arch.arch)):
             sh.rm('-rf', 'src/zope/interface/tests', 'src/zope/interface/common/tests')
+            #sh.touch('src/zope/__init__.py')
 
 
 recipe = ZopeInterfaceRecipe()
