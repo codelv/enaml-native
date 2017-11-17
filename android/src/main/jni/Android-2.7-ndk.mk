ff0d06aa3bcffb064d546637b4f46fe08842977f
frmdstryr <frmdstryr@gmail.com>
2017-11-17 08:31:17 -0500

    Use Android.mk that's compatible with both crystax and python2 recipes

--- android/src/main/jni/Android-2.7-ndk.mk
+++ android/src/main/jni/Android-2.7-ndk.mk
@@ -1,7 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 #: Set by build script to <app>/build/python/
-PYTHON_PATH := ../../../../../../build/python
-
+#: If unchanged it assumes enaml-native is installed
+#: under venv/packages/enaml-native/
+PYTHON_PATH := $(call my-dir)/../../../../../../../build/python
 
 # Build libpybridge.so
 
@@ -9,14 +10,15 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := pybridge
 LOCAL_SRC_FILES := pybridge.c
 LOCAL_LDLIBS := -llog
-LOCAL_SHARED_LIBRARIES := python2.7
+LOCAL_CFLAGS := -I$(PYTHON_PATH)/$(TARGET_ARCH_ABI)/include/python2.7
+LOCAL_LDFLAGS := -L$(PYTHON_PATH)/$(TARGET_ARCH_ABI)/modules/ -lpython2.7
+#LOCAL_SHARED_LIBRARIES := python2.7
 include $(BUILD_SHARED_LIBRARY)
 
 
 # Include libpython2.7.so
-
 include $(CLEAR_VARS)
 LOCAL_MODULE    := python2.7
 LOCAL_SRC_FILES := $(PYTHON_PATH)/$(TARGET_ARCH_ABI)/modules/libpython2.7.so
-LOCAL_EXPORT_CFLAGS := -I $(PYTHON_PATH)/$(TARGET_ARCH_ABI)/include/
+LOCAL_EXPORT_CFLAGS := -I$(PYTHON_PATH)/$(TARGET_ARCH_ABI)/include/python2.7
 include $(PREBUILT_SHARED_LIBRARY)
